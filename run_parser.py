967aed0b6785c14f7b10636b0f743e41925d8b71
maryvito <maryv2@yandex.ru>
2017-07-22 15:59:27 +0300

    убраны лишние комменты

--- run_parser.py
+++ run_parser.py
@@ -2,13 +2,12 @@ from parsers import hh, superjob
 from models import Resume, Keywords, db_session
 
 
-# hh.py
 def parse_resume_hh(db_session):
     hh.parse_resumes_from_hh(db_session)
 
+
 def parse_resume_superjob(db_session):
     superjob.parse_resumes_from_superjob(db_session)
-
 
 
 if __name__ == "__main__":
